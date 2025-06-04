a7578cde5362129f9956e489021ae7557c0d85e8
Mauricio L <maurolanm@gmail.com>
2025-06-04 16:41:52 -0500

    get user completed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from database import user_collection
+from nosql_example.database import user_collection
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
@@ -8,6 +8,8 @@ class User(BaseModel):
     name: str
     email: str
 
+class UserResponse(User):
+    id: str
 
 @app.get("/users")
 def read_users() ->list[User]:
@@ -21,3 +23,32 @@ def create_user(
     result = user_collection.insert_one(
         user.model_dump(exclude_none=True)
     )
+
+    user_response = UserResponse(
+        id = str(result.inserted_id),
+        **user.model_dump()
+    )
+    return user_response
+
+from bson import ObjectId
+
+@app.get("/user")
+def get_user(user_id: str):
+    db_user = user_collection.find_one(
+        {
+            "_id":ObjectId(user_id)
+            if ObjectId.is_valid(user_id)
+            else None
+        }
+    )
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+    user_response = UserResponse(
+        id=str(db_user["_id"]),
+        **db_user
+    )
+    return user_response
